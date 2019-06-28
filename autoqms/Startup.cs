043314eeff91f545f3c55db7859dbde1ea8d6cc8
o-smirnov <oleg.smirnov@firstlinesoftware.com>
2019-06-29 00:28:07 +0300

    Project Init

---
+++ autoqms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(autoqms.Startup))]
+namespace autoqms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
